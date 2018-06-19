5136e89c7241addcd0b5464b40c67c9926603951
synical <sjohnsondot@gmail.com>
2018-06-19 13:57:10 +0000

    Add TODO to page_cache_info

--- page_cache_info/page_cache_info.c
+++ page_cache_info/page_cache_info.c
@@ -6,6 +6,17 @@ MODULE_AUTHOR("synical");
 MODULE_DESCRIPTION("page_cache_info");
 MODULE_VERSION("0.01");
 
+/*
+   TODO
+    - /proc/page_cache_info/file
+        - Read in filename via write op
+            - Print info about address_space
+            - Print info about buffer page/head
+    - /proc/page_cache_info/global
+        - Give overall info about page cache
+            - Amount used
+*/
+
 static int __init page_cache_info_init(void) {
     return 0;
 }
